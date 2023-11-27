453d544dba4b46e547611352d5a8272e41708fbb
crwaskar <104088193+crwaskar@users.noreply.github.com>
2023-11-27 11:20:08 +0530

    Update utils.js

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://ulka2n3quqdr4kdj742k3bhrue0svhnw.lambda-url.us-west-1.on.aws/task";
+  "https://xrqwjdemsjkzhq3fvpmelbbj3m0gznal.lambda-url.us-west-1.on.aws/task";
